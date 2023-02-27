9e074e0cce50f42e3f17df6402436c77526a5b49
luizaalanis <luizalanisantos@gmail.com>
2023-02-27 17:47:20 -0300

    Return commom values in both lists

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def intersect_lists(list_1, list_2):
+    """
+    Retorna os valores em comum entre duas listas.
+    """
+    return list(set(list_1) & set(list_2))
 
 
-# Press the green button in the gutter to run the script.
+def format_input(list_input):
+    # Separar elementos da lista usando o método split()
+    elements = list_input.split(",")
+
+    # Inicializar um novo array de strings
+    array_strings = []
+
+    # Adicionar cada elemento da lista ao array de strings
+    for element in elements:
+        array_strings.append(str(element))
+
+    return array_strings
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    list_1 = format_input(input("Enter the first header list: "))
+    list_2 = format_input(input("Enter the second header list: "))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(intersect_lists(list_1, list_2))
